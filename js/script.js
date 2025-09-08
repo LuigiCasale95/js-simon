8411230af983060c4a90e7ed691d704ace98393b
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-08 17:24:47 +0200

    Creazione countdown e azioni da emettere al termine

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 /* Dichiarazione INPUT */
 const button = document.querySelector(".btn-primary")
-const RandomNumList = document.getElementById("numbers-list")
+const randomNumList = document.getElementById("numbers-list")
 const countdown = document.getElementById("countdown")
 const  instructions = document.getElementById("instructions")
-const numUser = document.querySelectorAll("form-control")
+const numUser = document.getElementById("answers-form")
 
+let second = 3
+countdown.innerText = second--
 /* Array vuoto */
    let arrayRandomNum = []
 
 /* generazione array con 5 num. casuali */
    arrayRandomNum = arrayAddNum(10, 50, 5);
    console.log(arrayRandomNum);
-   
-/* far visualizzare i numeri per 30 secondi  */
+/* Far apparire i numeri in HTML */
+   for (i = 0; i < arrayRandomNum.length; i++) {
+    let elementLi = arrayRandomNum[i]
+    randomNumList.innerHTML = arrayRandomNum
+   }
+/* Countdown di 30 secondi */
+/* Far scomparire i numeri dopo 30 secondi  */
+const countdown30 = setInterval(function() {
+    if(second === 0){
+        numUser.classList.remove("d-none")
+        randomNumList.classList.add("d-none")
+        countdown.classList.add("d-none")
+        instructions.innerHTML = "<h2>Ricordi che numeri erano?</h2>"
+
+    } else {
+        countdown.innerText = second--
+    }
+
+}, 1000)
 
 /* confronto numeri casuali  con numeri scelti dall'utente */
+
+
 
 /* dichiarare quanti numeri sono uguali a quelli casuali apparsi */
 
