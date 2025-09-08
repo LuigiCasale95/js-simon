826ab13f2f426bbd9928622c553bc7d61dafbc48
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-08 22:29:07 +0200

    correzioni e creazione di array dei numeri scelti dall'utente

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@ const button = document.querySelector(".btn-primary")
 const randomNumList = document.getElementById("numbers-list")
 const countdown = document.getElementById("countdown")
 const  instructions = document.getElementById("instructions")
-const numUser = document.getElementById("answers-form")
+const myForm = document.getElementById("answers-form")
+const msn = document.getElementById("message")
+
+let numUser1 = document.querySelector(".form-control1")
+let numUser2 = document.querySelector(".form-control2")
+let numUser3 = document.querySelector(".form-control3")
+let numUser4 = document.querySelector(".form-control4")
+let numUser5 = document.querySelector(".form-control5")
+
+/* numUser1 = numUser1.value
+numUser2 = numUser2.value
+numUser3 = numUser3.value
+numUser4 = numUser4.value
+numUser5 = numUser5.value
+ */
+console.log(numUser1);
 
 let second = 3
 countdown.innerText = second--
@@ -22,7 +37,7 @@ countdown.innerText = second--
 /* Far scomparire i numeri dopo 30 secondi  */
 const countdown30 = setInterval(function() {
     if(second === 0){
-        numUser.classList.remove("d-none")
+        myForm.classList.remove("d-none")
         randomNumList.classList.add("d-none")
         countdown.classList.add("d-none")
         instructions.innerHTML = "<h2>Ricordi che numeri erano?</h2>"
@@ -33,11 +48,26 @@ const countdown30 = setInterval(function() {
 
 }, 1000)
 
-/* confronto numeri casuali  con numeri scelti dall'utente */
+/* Array con i numeri scelti dall'utente */
+myForm.addEventListener("submit",
+     function (evento){
+    evento.preventDefault();
+        let numUtente = []
+
+        numUtente.push(numUser1.value)
+        numUtente.push(numUser2.value)
+        numUtente.push(numUser3.value)
+        numUtente.push(numUser4.value)
+        numUtente.push(numUser5.value)
+
+        console.log(numUtente);
+/* Confrontare ogni numero di ArrayCasuale con numeri ArrayUtente */
+        let numVincenti = []
+        console.log(numVincenti);
+})
 
 
 
-/* dichiarare quanti numeri sono uguali a quelli casuali apparsi */
 
 
 
