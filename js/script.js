d31b39112ca4e1d8b10bd982a6b74a22b4a2eb35
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-08 15:48:40 +0200

    base esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Dichiarazione INPUT */
+
+
+/* generazione array con 5 num. casuali */
+
+/* far visualizzare i numeri per 30 secondi  */
+
+/* confronto numeri casuali  con numeri scelti dall'utente */
+
+/* dichiarare quanti numeri sono uguali a quelli casuali apparsi */
