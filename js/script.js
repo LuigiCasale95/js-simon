385e6380afdb8b14ceef222cae05afcb0c7d3c25
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-08 15:56:32 +0200

    Dichiarazione variabili

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 /* Dichiarazione INPUT */
-
+const button = document.querySelector(".btn-primary")
+const RandomNumList = document.getElementById("numbers-list")
+const countdown = document.getElementById("countdown")
+const  instructions = document.getElementById("instructions")
+const numUser = document.querySelectorAll("form-control")
 
 /* generazione array con 5 num. casuali */
 
